49d785208353f49e4e50f644ca862d2b90b1b7ce
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-05-17 16:25:16 +0530

    commit development branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,42 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        'ws-blue': {
-          100: '#E6F0FF',
-          200: '#BFDAFF',
-          300: '#99C3FF',
-          400: '#4D97FE',
-          500: '#006AFE',
-          600: '#005FE5',
-          700: '#004098',
-          800: '#003072',
-          900: '#00204C',
-        },
-      }
-    },
-  },
-  variants: {},
-  plugins: [],
-  purge: process.env.NODE_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
-}
+	theme: {
+		extend: {
+			colors: {
+				skyBlue: "#E0F9F9",
+				turquoise: "#39CCCC",
+				lightCyan: "#EDFFFD",
+				"ws-blue": {
+					100: "#E6F0FF",
+					200: "#BFDAFF",
+					300: "#99C3FF",
+					400: "#4D97FE",
+					500: "#006AFE",
+					600: "#005FE5",
+					700: "#004098",
+					800: "#003072",
+					900: "#00204C"
+				}
+			},
+			screens: {
+				"a-vp": "320px",
+				"b-vp": "480px",
+				"c-vp": "600px",
+				"d-vp": "768px",
+				"e-vp": "992px",
+				"f-vp": "1200px"
+			}
+		},
+		maxWidth: {
+			wrap: "1110px"
+		}
+	},
+	variants: {},
+	plugins: [],
+	purge:
+		process.env.NODE_ENV === "production"
+			? {
+					enabled: true,
+					content: ["src/**/*.njk", "src/**/*.js"]
+			  }
+			: {}
+};
