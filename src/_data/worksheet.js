08ea802e2f5345a64edf8352ce1dc7fba4b09d7d
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-05-19 13:51:59 +0530

    worksheet info modal layout

--- src/_data/worksheet.js
+++ src/_data/worksheet.js
@@ -12,31 +12,26 @@ module.exports = {
 		},
 		{
 			id: 3,
-
 			name: "Grade 2",
 			ageGroup: "Age 7-8 years old"
 		},
 		{
 			id: 4,
-
 			name: "Grade 3",
 			ageGroup: "Age 8-9 years old"
 		},
 		{
 			id: 5,
-
 			name: "Grade 4",
 			ageGroup: "Age 9-10 years old"
 		},
 		{
 			id: 6,
-
 			name: "Grade 5",
 			ageGroup: "Age 10-11 years old"
 		},
 		{
 			id: 7,
-
 			name: "Grade 6",
 			ageGroup: "Age 11-12 years old"
 		}
