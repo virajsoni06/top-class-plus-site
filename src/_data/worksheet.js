09c326342ccf796012459e3005f8713d8d8d4291
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-05-19 11:41:26 +0530

    UI purchase worksheet redesign

---
+++ src/_data/worksheet.js
@@ -0,0 +1,44 @@
+module.exports = {
+	worksheets: [
+		{
+			id: 1,
+			name: "Kindergarden Worksheet Bundle",
+			ageGroup: "Age 5-6 years old"
+		},
+		{
+			id: 2,
+			name: "Grade 1",
+			ageGroup: "Age 6-7 years old"
+		},
+		{
+			id: 3,
+
+			name: "Grade 2",
+			ageGroup: "Age 7-8 years old"
+		},
+		{
+			id: 4,
+
+			name: "Grade 3",
+			ageGroup: "Age 8-9 years old"
+		},
+		{
+			id: 5,
+
+			name: "Grade 4",
+			ageGroup: "Age 9-10 years old"
+		},
+		{
+			id: 6,
+
+			name: "Grade 5",
+			ageGroup: "Age 10-11 years old"
+		},
+		{
+			id: 7,
+
+			name: "Grade 6",
+			ageGroup: "Age 11-12 years old"
+		}
+	]
+};
