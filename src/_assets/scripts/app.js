a82a7f6a479b2472aa8d7febdea759419fa4e073
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-05-27 09:16:07 +0530

    find geolocation of users to display currency

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,1 +1,23 @@
-console.log(`I was loaded at ${Date(Date.now()).toString()}`);
+// console.log(`I was loaded at ${Date(Date.now()).toString()}`);
+
+const currency = document.querySelectorAll(".currency");
+
+fetch("https://extreme-ip-lookup.com/json/")
+	.then(res => res.json())
+	.then(response => {
+		if (response.country === "India") {
+			currency.forEach(function(item) {
+				item.innerHTML = "₹";
+			});
+		} else if (response.country === "USA") {
+			currency.innerHTML = "K";
+		} else if (response.country === "UK") {
+			currency.innerHTML = "L";
+		} else {
+			currency.innerHTML = "K";
+		}
+		console.log("Country: ", response);
+	})
+	.catch((data, status) => {
+		console.log("Request failed");
+	});
