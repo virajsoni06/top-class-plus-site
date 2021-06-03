c505ee72a10d433f6ec6d1fbe509b0d6b6243dc7
Viraj Soni <viraj.viraaj@gmail.com>
2021-06-03 19:47:26 +0530

    Adding vercel, removing netlify

--- .eleventy.js
+++ .eleventy.js
@@ -2,22 +2,22 @@ const filters = require("./utils/filters.js");
 const transforms = require("./utils/transforms.js");
 const collections = require("./utils/collections.js");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static");
 
 	// Filters
-	Object.keys(filters).forEach(filterName => {
+	Object.keys(filters).forEach((filterName) => {
 		eleventyConfig.addFilter(filterName, filters[filterName]);
 	});
 
 	// Transforms
-	Object.keys(transforms).forEach(transformName => {
+	Object.keys(transforms).forEach((transformName) => {
 		eleventyConfig.addTransform(transformName, transforms[transformName]);
 	});
 
 	// Collections
-	Object.keys(collections).forEach(collectionName => {
+	Object.keys(collections).forEach((collectionName) => {
 		eleventyConfig.addCollection(collectionName, collections[collectionName]);
 	});
 
@@ -29,12 +29,12 @@ module.exports = function(eleventyConfig) {
 			input: "src/",
 			output: "dist",
 			includes: "_includes",
-			layouts: "_layouts"
+			layouts: "_layouts",
 		},
 		templateFormats: ["html", "md", "njk"],
 		htmlTemplateEngine: "njk",
 
 		// 1.1 Enable eleventy to pass dirs specified above
-		passthroughFileCopy: true
+		passthroughFileCopy: true,
 	};
 };
