a2ee2e751a37eff7674fd7b4ec14aad6c52ae71e
sadiq-translatez <mohammed@translatez.co>
2021-06-11 15:55:14 +0530

    removed paddle related configs

--- src/_data/worksheet.js
+++ src/_data/worksheet.js
@@ -4,50 +4,43 @@ module.exports = {
 			id: 1,
 			name: "Kindergarden Worksheet Bundle",
 			price: "99.00",
-			ageGroup: "Age 5-6 years old",
-			productId: 12526
+			ageGroup: "Age 5-6 years old"
 		},
 		{
 			id: 2,
 			name: "Grade 1 Bundle",
 			price: "99.00",
-			ageGroup: "Age 6-7 years old",
-			productId: 12529
+			ageGroup: "Age 6-7 years old"
 		},
 		{
 			id: 3,
 			name: "Grade 2 Bundle",
 			price: "99.00",
-			ageGroup: "Age 7-8 years old",
-			productId: 12530
+			ageGroup: "Age 7-8 years old"
 		},
 		{
 			id: 4,
 			name: "Grade 3 Bundle",
 			price: "99.00",
-			ageGroup: "Age 8-9 years old",
-			productId: 12531
+			ageGroup: "Age 8-9 years old"
 		},
 		{
 			id: 5,
 			name: "Grade 4 Bundle",
 			price: "99.00",
-			ageGroup: "Age 9-10 years old",
-			productId: 12532
+			ageGroup: "Age 9-10 years old"
 		},
 		{
 			id: 6,
 			name: "Grade 5 Bundle",
 			price: "99.00",
-			ageGroup: "Age 10-11 years old",
-			productId: 12533
+			ageGroup: "Age 10-11 years old"
 		},
 		{
 			id: 7,
 			name: "Grade 6 Bundle",
 			price: "99.00",
-			ageGroup: "Age 11-12 years old",
-			productId: 12534
+			ageGroup: "Age 11-12 years old"
 		}
 	]
 };
